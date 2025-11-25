ce9c76d26a7dc80804fc7529ac76f23a32493cc7
Jung InHwan <sejicgndslj121@naver.com>
2025-11-25 17:06:34 +0900

    Update image_mood.py

--- datamood/image/image_mood.py
+++ datamood/image/image_mood.py
@@ -1,1 +1,26 @@
-pass
+import cv2
+from fer import FER
+
+class EmotionAnalyzer:
+    def __init__(self):
+        self.detector = FER(mtcnn=True)
+    
+    def analyze(self, image_path):
+        img=cv2.imread(image_path)
+
+        if img is None:
+            raise ValueError("이미지를 찾을 수 없습니다.")
+        
+        result = self.detector.detect_emotions(img)
+
+        if len(result)==0:
+            return {'message':'얼굴이 감지되지 않았습니다.'}
+        
+        emotions = result[0]['emotions']
+        emotion = max(emotions, key=emotions.get)
+
+        return{
+            'emotion':emotion,
+            'confidence':emotions[emotion],
+            'detail': emotions
+        }
